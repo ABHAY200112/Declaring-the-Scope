3f256096622f8ddc7abee028f762951636f137cc
ABHAY200112 <abhay.03-cse-16@mietjammu.in>
2017-09-20 20:45:10 +0530

    Update scope.cpp

--- scope.cpp
+++ scope.cpp
@@ -1,13 +1,12 @@
 //Author :- Abhay Jeet Zutshi
 #include<iostream>
 using namespace std;
-
 int main()
 {     // Declaring the scope
 int i = -1;
 for(int i=0;i<=10;i++)
-cout<<i<<" "<<&i<<endl;
-cout<<i<<&i<<endl; 
+cout<<i<<" "<<&i<<endl; // Print the Value of i from 1-10 and their address
+cout<<i<<&i<<endl; // Print the Value of First i and their address 
 return 1; // Returning the value
 }
 
